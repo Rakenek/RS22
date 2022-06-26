6235e1eeff6f62306bc93cfd3aaec72b651e517f
Rakenek <kamilotawski@gmail.com>
2022-06-26 22:07:30 +0200

    fc

---
+++ src/components/Auth/AuthForm.js
@@ -0,0 +1,82 @@
+import { useRef, useState } from "react";
+
+import classes from "./AuthForm.module.css";
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const passwordRef = useRef();
+  const emailRef = useRef();
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    const enteredPassword = passwordRef.current.value;
+    const enteredEmail = emailRef.current.value;
+
+    const Login = async () => {
+      try {
+        const response = await fetch(
+          `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=
+          AIzaSyAn8gUS0IVVofd1Fi5lSc1hf1g4lQhg0Zs`,
+          {
+            method: "POST",
+            body: JSON.stringify({
+              email: enteredEmail,
+              password: enteredPassword,
+              returnSecureToken: true,
+            }),
+            headers: { "Content-Type": "application/json" },
+          }
+        );
+        const data = await response.json();
+        setIsLoading(false);
+        if (data && data.error && data.error.message) {
+          alert(data.error.message);
+          throw new Error(data.error.message);
+        }
+      } catch (e) {
+        console.error(e.message);
+      }
+    };
+    setIsLoading(true);
+    if (isLogin) {
+    } else {
+      await Login();
+    }
+  };
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={onSubmitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input ref={emailRef} type="email" id="email" required />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="password">Your Password</label>
+          <input ref={passwordRef} type="password" id="password" required />
+        </div>
+        <div className={classes.actions}>
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
+          {isLoading && <p>Loading...</p>}
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? "Create new account" : "Login with existing account"}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
